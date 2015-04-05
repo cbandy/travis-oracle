600c01abe0f1f59f1d0b156156bf4ed562cdb94b
Chris Bandy <bandy.chris@gmail.com>
2015-04-05 05:40:37 +0000

    Run `apt-get update` before `apt-get install`

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ORACLE_RPM=`basename $ORACLE_FILE .zip`
 
+sudo apt-get -qq update
 sudo apt-get --no-install-recommends -qq install alien bc libaio1 unzip
 
 df -B1 /dev/shm | awk 'END { if ($1 != "shmfs" && $1 != "tmpfs" || $2 < 2147483648) exit 1 }' ||
