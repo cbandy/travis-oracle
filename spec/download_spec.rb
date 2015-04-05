92e510236e1b57ecfd7f66d95cca3adf8d6cf40b
Chris Bandy <bandy.chris@gmail.com>
2015-04-05 06:18:05 +0000

    Use modern RSpec syntax

--- spec/download_spec.rb
+++ spec/download_spec.rb
@@ -2,7 +2,7 @@
 describe 'download.sh' do
   context 'when ORACLE_FILE is defined', :if => ENV.has_key?('ORACLE_FILE') do
     it 'downloads from Oracle' do
-      File.exists?(ENV['ORACLE_FILE']).should be_true
+      expect(File).to exist(ENV['ORACLE_FILE'])
     end
   end
 end
--- spec/install_spec.rb
+++ spec/install_spec.rb
@@ -3,40 +3,40 @@ describe 'install.sh' do
   it 'mounts two gigabytes of shared memory at /dev/shm' do
     directory = '/dev/shm'
 
-    File.exist?(directory).should be_true
-    File.lstat(directory).should be_directory
-    `df -B1 #{directory}`.split("\n")[1].split(/\b/)[2].to_i.should >= 2147483648
+    expect(File).to exist(directory)
+    expect(File.lstat(directory)).to be_directory
+    expect(`df -B1 #{directory}`.split("\n")[1].split(/\b/)[2].to_i).to be >= 2147483648
   end
 
   it 'creates an executable that does nothing at /sbin/chkconfig' do
     executable = '/sbin/chkconfig'
 
-    File.exist?(executable).should be_true
-    File.lstat(executable).mode.should == '100744'.to_i(8)
-    IO.read(executable).should == "#!/bin/sh\n"
+    expect(File).to exist(executable)
+    expect(File.lstat(executable).mode).to eq('100744'.to_i(8))
+    expect(IO.read(executable)).to eq("#!/bin/sh\n")
   end
 
   it 'creates a directory at /var/lock/subsys' do
     directory = '/var/lock/subsys'
 
-    File.exist?(directory).should be_true
-    File.lstat(directory).should be_directory
+    expect(File).to exist(directory)
+    expect(File.lstat(directory)).to be_directory
   end
 
   context 'when ORACLE_FILE is defined', :if => ENV.has_key?('ORACLE_FILE') do
     it 'extracts an RPM' do
-      Dir.glob('*.rpm').should_not be_empty
+      expect(Dir.glob('*.rpm')).to_not be_empty
     end
 
     it 'generates a DEB' do
-      Dir.glob('*.deb').should_not be_empty
+      expect(Dir.glob('*.deb')).to_not be_empty
     end
 
     it 'installs Oracle Express Edition' do
-      File.exists?('/etc/init.d/oracle-xe').should be_true
+      expect(File).to exist('/etc/init.d/oracle-xe')
 
-      File.exists?(ENV['ORACLE_HOME']).should be_true
-      File.executable?(ENV['ORACLE_HOME'] + '/bin/sqlplus').should be_true
+      expect(File).to exist(ENV['ORACLE_HOME'])
+      expect(File).to be_executable(ENV['ORACLE_HOME'] + '/bin/sqlplus')
     end
   end
 
@@ -46,20 +46,20 @@ describe 'install.sh' do
 
       it 'grants normal access without password to the current user' do
         IO.popen([sqlplus, %w(-L -S /)].flatten, 'w') { |io| io.puts 'exit' }
-        $?.should be_success
+        expect($?).to be_success
       end
 
       it 'grants DBA access without password to the current user' do
         IO.popen([sqlplus, %w(-L -S / AS SYSDBA)].flatten, 'w') { |io| io.puts 'exit' }
-        $?.should be_success
+        expect($?).to be_success
       end
     end
 
     describe 'library access' do
-      require 'oci8'
+      before(:context) { require 'oci8' }
 
       it 'grants normal access without password to the current user' do
-        OCI8.new('/').exec('SELECT 1 FROM DUAL') { |row| row.should == [1] }
+        OCI8.new('/').exec('SELECT 1 FROM DUAL') { |row| expect(row).to eq([1]) }
       end
     end
   end
