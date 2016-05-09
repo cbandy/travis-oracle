ba6220959305306a9bda9dcf7cd9e4531addc4fe
John Vandenberg <jayvdb@gmail.com>
2016-05-09 17:59:15 +0700

    Download files into ORACLE_DOWNLOAD_DIR
    This allows download files to be located outside of the source tree,
    and thus in a cacheable directory. Cached files are made available
    to pull requests which are otherwise unable to access the hidden or
    encrypted variables for login.
    
    https://docs.travis-ci.com/user/caching/

--- download.sh
+++ download.sh
@@ -1,12 +1,21 @@
 #!/bin/sh -e
+# vim: set et sw=2 ts=2:
 
 [ -n "$ORACLE_COOKIE" ] || { echo "Missing ORACLE_COOKIE environment variable!"; exit 1; }
 [ -n "$ORACLE_FILE" ] || { echo "Missing ORACLE_FILE environment variable!"; exit 1; }
+
+ORACLE_DOWNLOAD_FILE="$(basename "$ORACLE_FILE")"
+
+if [ -n "$ORACLE_DOWNLOAD_DIR" ]; then
+  mkdir -p "$ORACLE_DOWNLOAD_DIR"
+  ORACLE_DOWNLOAD_FILE="$(readlink -f "$ORACLE_DOWNLOAD_DIR")/$ORACLE_DOWNLOAD_FILE"
+fi
 
 cd "$(dirname "$(readlink -f "$0")")"
 
 npm install bluebird node-phantom-simple
 
+export ORACLE_DOWNLOAD_FILE
 export COOKIES='cookies.txt'
 export USER_AGENT='Mozilla/5.0'
 
